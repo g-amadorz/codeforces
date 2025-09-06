0c53e51691d07cf1ea32d36d273d155c8e2ad708
g-amadorz <gabe.amadorz@gmail.com>
2025-09-06 09:36:24 -0700

    first commit

---
+++ round_1043/q1.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+using namespace std;
+void solve() {
+  int n;
+  cin >> n;
+
+  string a;
+  cin >> a;
+
+  int m;
+  cin >> m;
+
+  string b;
+  cin >> b;
+
+  for (int i = 1; i < m + 1; i++) {
+
+    if (i % 2 == 1) {
+    }
+  }
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
